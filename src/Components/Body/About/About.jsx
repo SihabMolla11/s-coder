480a47f65382647524334a0e6048ccd55f7c8fed
SihabMolla11 <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>
2025-04-27 00:24:24 +0600

    update resume

--- src/Components/Body/About/About.jsx
+++ src/Components/Body/About/About.jsx
@@ -33,35 +33,35 @@ const About = () => {
         />
 
         <p className="text-lg text-white">
-          From the moment I wrote my first line of code, I knew this was more than just a career—it
+          From the moment I wrote my first line of code, I knew this was more than just a career it
           was a passion. I'm a full-stack developer who thrives in the world of web development,
           turning ideas into reality with clean, efficient, and scalable code. My love for
           programming isn't just about writing functions or fixing bugs; it's about the challenges
           it throws at me every single day. I don't just solve problems—I embrace them, learn from
           them, and grow stronger with each challenge. <br />
           <br />I specialize in Next.js, React.js, TypeScript, JavaScript, NestJS, Express.js, PHP,
-          Laravel, and MongoDB. But my expertise doesn’t stop there. I bring aesthetic precision to
-          frontend development with frameworks like Tailwind CSS, Bootstrap, Daisy UI, Ant Design,
-          ShadCN, and Material UI, ensuring that every UI is seamless, responsive, and visually
-          stunning. On the backend, I architect robust, scalable, and high-performance applications
-          with modern technologies, delivering real-world impact.
+          Laravel, Prisma, Mongoose, PostgreSQL, MongoDB and My SQL. But my expertise doesn’t stop
+          there. I bring aesthetic precision to frontend development with frameworks like Tailwind
+          CSS, Bootstrap, Hero UI, Ant Design, ShadCN, and Material UI, ensuring that every UI is
+          seamless, responsive, and visually stunning. On the backend, I architect robust, scalable,
+          and high performance applications with modern technologies, delivering real world impact.
           <br />
           <br />
-          Why I Love What I Do Programming is more than just a skill—it’s a way of thinking. It’s a
+          Why I Love What I Do Programming is more than just a skill it’s a way of thinking. It’s a
           game where every bug is a puzzle, and every solution is a victory. I love taking on new
-          challenges, pushing myself beyond limits, and diving into the unknown. The ever-evolving
+          challenges, pushing myself beyond limits, and diving into the unknown. The ever evolving
           nature of this field excites me, and I find joy in learning, experimenting, and building
           every day. <br />
           <br />
-          Be it databases like MongoDB, PostgreSQL, MySQL, and Prisma, or API testing with Postman,
-          I am always ready to explore deeper. My hunger for learning keeps me going, and my
-          dedication ensures that I deliver the best possible solutions to every problem I
+          Be it databases like MongoDB, PostgreSQL, MySQL, and Prisma, or API testing with Postman
+          and Swagger, I am always ready to explore deeper. My hunger for learning keeps me going,
+          and my dedication ensures that I deliver the best possible solutions to every problem I
           encounter. <br />
           <br />
-          Let’s Connect I believe in collaboration and love to connect with like-minded developers,
+          Let’s Connect I believe in collaboration and love to connect with like minded developers,
           businesses, and enthusiasts who share my passion for coding. If you’re looking for someone
           who not only writes code but lives it, feel free to reach out. <br />
-          <br />I look forward to the next challenge—bring it on! 🚀
+          <br />I look forward to the next challenge bring it on! 🚀
         </p>
       </div>
     </div>
--- src/Components/DownloadResume/DownloadResume.jsx
+++ src/Components/DownloadResume/DownloadResume.jsx
@@ -4,7 +4,11 @@ const DownloadResume = () => {
   return (
     <>
       <button className="border-2 mt-5 rounded-lg hover:bg-gradient-to-r to-[#D66CFF] from-[#860C78] p-3 text-md border-[#860C78] text-[#860C78] font-bold hover:text-white  bg-[#efcafd]">
-        <a className="flex gap-2" href="Sihab Uddin Molla's Resume.pdf" download>
+        <a
+          className="flex gap-2"
+          href="Resume Of Full Stack Developer Sihab Uddin Molla.docx.pdf"
+          download
+        >
           Download resume <BiArrowToBottom className="text-2xl" />
         </a>
       </button>
