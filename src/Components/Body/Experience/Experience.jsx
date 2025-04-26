4460c3c6a2bcba343c3cdd74bf1dfa858de6456d
SihabMolla11 <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>
2025-04-26 20:16:16 +0600

    update experirence

--- src/Components/Body/Experience/Experience.jsx
+++ src/Components/Body/Experience/Experience.jsx
@@ -5,92 +5,80 @@ import { Link } from "react-router-dom";
 import { experiences } from "../../../data/experience";
 
 const Experience = () => {
-    return (
-        <div data-aos="fade-up" className="my-container mt-10 mb-20 ">
-            <h2 className="text-4xl text-center font-bold text-[#dbdada] uppercase">
-                EXPERIENCE
-            </h2>
-            {experiences?.map((experience, index) => (
-                <div
-                    key={index}
-                    className="bg-[#dbdada] rounded-xl p-8 mt-8 space-y-6"
+  return (
+    <div data-aos="fade-up" className="my-container mt-10 mb-20 ">
+      <h2 className="text-4xl text-center font-bold text-[#dbdada] uppercase">EXPERIENCE</h2>
+      {experiences?.map((experience, index) => (
+        <div key={index} className="bg-[#dbdada] rounded-xl p-8 mt-8 space-y-6">
+          <div className="flex flex-col md:flex-row gap-8 items-center justify-between">
+            <Link
+              className="cursor-pointer max-w-fit bg-[#460c41] flex items-center justify-start gap-4 rounded-full p-1 pr-6 "
+              to={experience?.website}
+              target="_blank"
+            >
+              <div className="h-14 w-14 p-4 border bg-[#61616173] border-[#8B1381] rounded-full">
+                <img
+                  className="w-full h-full"
+                  height={100}
+                  width={100}
+                  src={experience?.logo}
+                  alt={experience?.name}
+                />
+              </div>
+              <div>
+                <h3 className="text-xl text-white font-bold">{experience?.name}</h3>
+                <p className="flex items-center justify-start gap-1 text-white">
+                  <ImLocation2 />
+                  <span className="text-sm font-semibold">{experience?.location}</span>
+                </p>
+              </div>
+            </Link>
+            <button
+              type="button"
+              className="uppercase font-bold py-1 px-4 rounded-full flex items-center  gap-2 bg-[#d56cff63]"
+            >
+              <FaRegCalendarAlt className="text-black" />
+              <span className="text-black">
+                {experience?.started} - {experience?.end ? experience?.end : "Present"}
+              </span>
+            </button>
+          </div>
+
+          <div className="flex items-center justify-start gap-4">
+            <div className="h-14 w-14 rounded-full border border-[#D66CFF] overflow-hidden ">
+              <img
+                height={70}
+                website={70}
+                src={experience?.image}
+                alt="Sihab Uddin Molla"
+                className="h-full w-full"
+              />
+            </div>
+            <h3 className="text-2xl font-bold capitalize text-black">
+              {experience?.position}
+            </h3>
+          </div>
+
+          <div>
+            <ul>
+              {experience?.responsibilities?.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex items-start gap-6 pb-2 font-medium text-[#000000af]"
                 >
-                    <div className="flex flex-col md:flex-row gap-8 items-center justify-between">
-                        <Link
-                            className="cursor-pointer max-w-fit bg-[#460c41] flex items-center justify-start gap-4 rounded-full p-1 pr-6 "
-                            to={experience?.website}
-                            target="_blank"
-                        >
-                            <div className="h-14 w-14 p-4 border bg-[#61616173] border-[#8B1381] rounded-full">
-                                <img
-                                    className="w-full h-full"
-                                    height={100}
-                                    width={100}
-                                    src={experience?.logo}
-                                    alt={experience?.name}
-                                />
-                            </div>
-                            <div>
-                                <h3 className="text-xl text-white font-bold">
-                                    {experience?.name}
-                                </h3>
-                                <p className="flex items-center justify-start gap-1 text-white">
-                                    <ImLocation2 />
-                                    <span className="text-sm font-semibold">
-                                        {experience?.location}
-                                    </span>
-                                </p>
-                            </div>
-                        </Link>
-                        <button
-                            type="button"
-                            className="uppercase font-bold py-1 px-4 rounded-full flex items-center  gap-2 bg-[#d56cff63]"
-                        >
-                            <FaRegCalendarAlt className="text-black"/>
-                            <span className="text-black">
-                                {experience?.started} -{" "}
-                                {experience?.end ? experience?.end : "Present"}
-                            </span>
-                        </button>
-                    </div>
+                  <span className="mt-[5px]">
+                    <FiCheckCircle className="text-green-600" />
+                  </span>
 
-                    <div className="flex items-center justify-start gap-4">
-                        <div className="h-14 w-14 rounded-full border border-[#D66CFF] overflow-hidden ">
-                            <img
-                                height={70}
-                                website={70}
-                                src={experience?.image}
-                                alt="Sihab Uddin Molla"
-                                className="h-full w-full"
-                            />
-                        </div>
-                        <h3 className="text-2xl font-bold capitalize text-black">
-                            {experience?.position}
-                        </h3>
-                    </div>
-
-                    <div>
-                        <ul>
-                            {experience?.responsibilities?.map(
-                                (item, index) => (
-                                    <li
-                                        key={index}
-                                        className="flex items-start gap-6 pb-2 font-medium text-[#000000af]"
-                                    >
-                                        <span className="mt-[5px]">
-                                            <FiCheckCircle className="text-green-600" />
-                                        </span>
-
-                                        <span>{item?.text}</span>
-                                    </li>
-                                )
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            ))}
+                  <span className="text-black font-medium">{item?.text}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Experience;
--- src/data/experience.js
+++ src/data/experience.js
@@ -1,32 +1,41 @@
 export const experiences = [
-    {
-        logo: "/image/experience/bigpixel_bd_logo.png",
-        name: "BITPIXEL.",
-        position: "Full Stack Developer",
-        started: "November 2023",
-        end: null,
-        location: "Dhaka, Bangladesh",
-        image: "/image/me/sihab_uddin_molla.png",
-        website: "http://bitpixelbd.com",
-        responsibilities: [
-            {
-                text: "Design and development over 7+ completed React js and Next Js project. Showcasing proficiency in front-end design and development",
-            },
-            {
-                text: "Creating APIs based on frontend requirements and formatting backend data to ensure compatibility and optimal use by the frontend.",
-            },
-            {
-                text: "Demonstrated experience in design and building reusable components and pages, ensuring responsiveness and adherence to design specification.",
-            },
-            // {
-            //     text: "Shared knowledge expertise by conducing training section on Javascript, Responsive Web Design, and React js for trainees and junior developers, contributing to skill development with my company.",
-            // },
-            {
-                text: "Proficiently utilized JavaScript, React.js, and Next.js for front-end development, expertly managing CRUD operations with NestJS, Prisma, and PostgreSQL to efficiently handle and manipulate data.",
-            },
-            {
-                text: "Successfully deployed applications on Vercel and databases deployed  on Render, ensuring optimal performance and accessibility.",
-            },
-        ],
-    },
+  {
+    logo: "/image/experience/bigpixel_bd_logo.png",
+    name: "BITPIXEL.",
+    position: "Full Stack Developer",
+    started: "November 2023",
+    end: null,
+    location: "Dhaka, Bangladesh",
+    image: "/image/me/sihab_uddin_molla.png",
+    website: "http://bitpixelbd.com",
+    responsibilities: [
+      {
+        text: "Design and implement responsive user interfaces using React, Next.js, Javascript, Tailwind CSS, CSS, and HTML5.",
+      },
+      {
+        text: "Demonstrated experience in design and building reusable components and pages, ensuring responsiveness and adherence to design specification.",
+      },
+      {
+        text: "Build and maintain RESTful APIs using Node.js, Express Js, NestJs, Prisma, Mongoose and Laravel.",
+      },
+      {
+        text: "Implement data persistence & retrieval with MySQL, PostgreSQL and MongoDB.",
+      },
+      {
+        text: "Implement secure authentication and authorization using JWT, CryptoJS, and bcryptjs integrate NextAuth for Next.js applications.",
+      },
+      {
+        text: "Use Git and GitHub for version control; manage projects with Jira, Trello, and Asana",
+      },
+      {
+        text: "Proficient in front-end development using JavaScript, TypeScript, React.js, and Next.js, with expertise in managing CRUD operations and data handling through NestJS, Prisma, Express.js, Mongoose ODM, PostgreSQL, MongoDB, and MySQL.",
+      },
+      {
+        text: "Deploy applications using Docker; configure Nginx to connect projects with custom domains and manage DNS through Cloudflare.",
+      },
+      {
+        text: "Mentor and guide new interns by helping them understand and work with the company’s codebase, assisting them in learning new technologies, and supporting their integration into development projects.",
+      },
+    ],
+  },
 ];
